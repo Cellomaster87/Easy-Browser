a7eff3d0061cd574353bf73f6d4ef131ae9570ec
Michele Galvagno <michicello@gmail.com>
2019-02-25 18:07:15 +0100

    Initial Commit

---
+++ Easy Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Easy Browser
+//
+//  Created by Michele Galvagno on 25/02/2019.
+//  Copyright © 2019 Michele Galvagno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
